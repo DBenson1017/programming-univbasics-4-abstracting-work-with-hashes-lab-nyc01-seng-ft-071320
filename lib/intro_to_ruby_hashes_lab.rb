40c5606d0fb2c92c5b89a638057666fea58c472f
Devin Benson <devinbenson@gmail.com>
2020-06-26 19:28:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-  new_hash = {} 
-  new_hash[key] = value 
+
+  new_hash = {
+    key: value 
+  } 
   
 end
 
